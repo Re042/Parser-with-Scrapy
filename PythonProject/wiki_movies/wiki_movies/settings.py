b79c6b75427bd84be5b95581c470f039d1910c58
Re042 <119497871+Re042@users.noreply.github.com>
2026-01-30 20:15:28 +0300

    Update settings.py

--- PythonProject/wiki_movies/wiki_movies/settings.py
+++ PythonProject/wiki_movies/wiki_movies/settings.py
@@ -3,15 +3,12 @@ BOT_NAME = 'wiki_movies'
 SPIDER_MODULES = ['wiki_movies.spiders']
 NEWSPIDER_MODULE = 'wiki_movies.spiders'
 
-# Настройки для обхода сайта
 ROBOTSTXT_OBEY = True
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
 
-# Настройка задержки между запросами
 DOWNLOAD_DELAY = 0.5
 CONCURRENT_REQUESTS_PER_DOMAIN = 4
 
-# Настройка экспорта в CSV
 FEEDS = {
     'movies.csv': {
         'format': 'csv',
@@ -21,8 +18,8 @@ FEEDS = {
     }
 }
 
-# Включение middleware для случайных User-Agent
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+
 }
