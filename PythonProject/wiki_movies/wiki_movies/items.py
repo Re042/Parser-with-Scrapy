787483d755abbe68572ba2ae8e72edd68e4b755a
Re042 <119497871+Re042@users.noreply.github.com>
2026-01-30 20:13:51 +0300

    Update items.py

--- PythonProject/wiki_movies/wiki_movies/items.py
+++ PythonProject/wiki_movies/wiki_movies/items.py
@@ -1,10 +1,10 @@
 import scrapy
 
 class MovieItem(scrapy.Item):
-    title = scrapy.Field()        # Название фильма
-    genre = scrapy.Field()        # Жанр/жанры
-    director = scrapy.Field()     # Режиссёр
-    country = scrapy.Field()      # Страна
-    year = scrapy.Field()         # Год выпуска
-    wiki_url = scrapy.Field()     # URL страницы Wikipedia
-    qid = scrapy.Field()          # ID Wikidata (для fallback)
+    title = scrapy.Field()        
+    genre = scrapy.Field()        
+    director = scrapy.Field()     
+    country = scrapy.Field()      
+    year = scrapy.Field()         
+    wiki_url = scrapy.Field()     
+    qid = scrapy.Field()          
